5c08db65de3efb0d59a2905bff2112c953bc1d07
kumar-shridhar <shridhar.stark@gmail.com>
2019-04-09 18:03:40 +0900

    Sigmoid vs ReLU test with VGG on CIFAR10

---
+++ models/vgg.py
@@ -0,0 +1,72 @@
+'''VGG11/13/16/19 in Pytorch.'''
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+cfg = {
+    'VGG11': [64, 'M', 128, 'M', 256, 256, 'M', 512, 512, 'M', 512, 512, 'M'],
+    'VGG13': [64, 64, 'M', 128, 128, 'M', 256, 256, 'M', 512, 512, 'M', 512, 512, 'M'],
+    'VGG16': [64, 64, 'M', 128, 128, 'M', 256, 256, 256, 'M', 512, 512, 512, 'M', 512, 512, 512, 'M'],
+    'VGG19': [64, 64, 'M', 128, 128, 'M', 256, 256, 256, 256, 'M', 512, 512, 512, 512, 'M', 512, 512, 512, 512, 'M'],
+}
+
+
+class VGG_ReLU(nn.Module):
+    def __init__(self, vgg_name, nclass, img_width=32):
+        super(VGG_ReLU, self).__init__()
+        self.img_width = img_width
+        self.features = self._make_layers(cfg[vgg_name])
+        self.classifier = nn.Linear(512, nclass)
+
+    def forward(self, x):
+        out = self.features(x)
+        out = out.view(out.size(0), -1)
+        out = self.classifier(out)
+        return out, None # return None, to make it compatible with VGG_noise
+
+    def _make_layers(self, cfg):
+        layers = []
+        in_channels = 3
+        width = self.img_width
+        for x in cfg:
+            if x == 'M':
+                layers += [nn.MaxPool2d(kernel_size=2, stride=2)]
+                width = width // 2
+            else:
+                layers += [nn.Conv2d(in_channels, x, kernel_size=3, padding=1),
+                           nn.BatchNorm2d(x),
+                           nn.ReLU(inplace=True)]
+                in_channels = x
+        layers += [nn.AvgPool2d(kernel_size=width, stride=1)]
+        return nn.Sequential(*layers)
+
+
+class VGG_Sigmoid(nn.Module):
+    def __init__(self, vgg_name, nclass, img_width=32):
+        super(VGG_Sigmoid, self).__init__()
+        self.img_width = img_width
+        self.features = self._make_layers(cfg[vgg_name])
+        self.classifier = nn.Linear(512, nclass)
+
+    def forward(self, x):
+        out = self.features(x)
+        out = out.view(out.size(0), -1)
+        out = self.classifier(out)
+        return out, None # return None, to make it compatible with VGG_noise
+
+    def _make_layers(self, cfg):
+        layers = []
+        in_channels = 3
+        width = self.img_width
+        for x in cfg:
+            if x == 'M':
+                layers += [nn.MaxPool2d(kernel_size=2, stride=2)]
+                width = width // 2
+            else:
+                layers += [nn.Conv2d(in_channels, x, kernel_size=3, padding=1),
+                           nn.BatchNorm2d(x),
+                           nn.Sigmoid()]
+                in_channels = x
+        layers += [nn.AvgPool2d(kernel_size=width, stride=1)]
+        return nn.Sequential(*layers)
+
